5b458ecd33bfb7cda3781ae45184be5346daea8b
yashrajput2701 <yash.rajput@appinventiv.com>
2021-06-01 22:19:17 +0530

    name changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Ravi Rajput
+          Edit <code>src/App.js</code> cruiser
         </p>
         <a
           className="App-link"
