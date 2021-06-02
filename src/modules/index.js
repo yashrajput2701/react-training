755377e2855a7e0e09a0b8db42e692ad78f057d4
yashrajput2701 <yash.rajput@appinventiv.com>
2021-06-02 11:44:36 +0530

    added css file to home module for styling

---
+++ src/modules/index.js
@@ -0,0 +1,13 @@
+import React, {Component,} from 'react';
+import './index.css';
+class Home extends Component{
+    render() {
+        return(
+            <div>
+            <h1 className={"h1tag"}>{"Hello from Home"}</h1>
+            <h2>{"Hello from Home"}</h2>
+            </div>
+        )                                                                                                                                                                                                                                                                                      
+    }
+}
+export default Home;
